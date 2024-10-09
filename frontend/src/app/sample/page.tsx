4c61fef93b6ed78047f485b493b0afb17bd86886
ccbc7 <flexibleelbow@gmail.com>
2024-10-09 23:10:50 +0900

    axios install

--- frontend/src/app/sample/page.tsx
+++ frontend/src/app/sample/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import axios from "axios";
+
 export default function Sample() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <>
-      <h3>こんばんは</h3>
+      <div className="App">
+        <h3>こんばんは</h3>
+        <button onClick={onClickFetchData}>データ取得</button>
+      </div>
     </>
   );
 }
